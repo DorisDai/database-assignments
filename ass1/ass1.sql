2eddad63424be5714585e8de8d1104488ad4e39e
Doris <doris@DorisdeMacBook-Air.local>
2023-10-13 05:07:20 +1100

    ass1 Q4

--- ass1/ass1.sql
+++ ass1/ass1.sql
@@ -14,14 +14,16 @@
 
 ---- Q1
 --
-create or replace view Q1(state, nbreweries)as 
+create or replace view Q1(state, nbreweries) 
+as 
     select region as state, count(B.id) as nbreweries
     from Breweries B right join Locations L on B.located_in = L.id
     where L.country = 'Australia'
     group by region;
---
+
 ---- Q2
-create or replace view Q2(style,min_abv,max_abv) as
+create or replace view Q2(style,min_abv,max_abv) 
+as
 -- name should be double qupte single quote or no quotes?
 -- Answer:no single quotes, the other 2 are all good
     select name, min_abv, max_abv
@@ -31,7 +33,8 @@ create or replace view Q2(style,min_abv,max_abv) as
 --
 ---- Q3
 --
-create or replace view beerStyleRange(styleId, lo_abv, hi_abv) as
+create or replace view beerStyleRange(styleId, lo_abv, hi_abv) 
+as
     select style, min(ABV), max(ABV)
     from Beers 
     group by style;
@@ -39,16 +42,29 @@ create or replace view beerStyleRange(styleId, lo_abv, hi_abv) as
 create or replace view Q3(style, lo_abv, hi_abv, min_abv, max_abv)
 as
     select name, lo_abv, hi_abv, min_abv, max_abv
+    -- when attribute names not same, no need to use Table.attribute
     from Styles join beerStyleRange on id = styleId
     where (lo_abv < min_abv or hi_abv > max_abv) and min_abv != max_abv;
 ;
 --
 ---- Q4
 --
---create or replace view Q4(...)
---as
---...
---;
+create or replace view breweryRating(validBrewery,avgRating)
+as
+    select brewery, avg(rating::float)::numeric(3,1)
+    from Brewed_by join Beers on beer = id
+    group by brewery
+    -- difference between having and where:
+    -- use where to check wheather each tuple should be included
+    having count(rating) > 4;
+
+create or replace view Q4(brewery,rating)
+as
+    select validBrewery, avgRating
+    from breweryRating
+    where avgRating = (select max(avgRating) from breweryRating);
+
+;
 --
 ---- Q5
 --
