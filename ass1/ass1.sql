6df8b263028ef607e26800aec966ee82ea70e279
DorisDai <dddorishhh@outlook.com>
2023-10-13 23:53:47 +1100

    ass1 q5

--- ass1/ass1.sql
+++ ass1/ass1.sql
@@ -72,9 +72,9 @@ as
 create or replace function
    Q5(pattern text) returns table(beer text, container text, std_drinks numeric)
 as $$
-    select name, volume || ' ' || sold_in, volume * ABV * 0.0008::numeric(2,1)
+    select name, volume || 'ml ' || sold_in, (volume * ABV * 0.0008)::numeric(2,1)
     from Beers
-    where name ~ ('.*' || pattern || '.*');
+    where name ~* ('.*' || pattern || '.*');
 $$
 language sql ;
 --
