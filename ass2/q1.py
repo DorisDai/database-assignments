12deb947c35f1fdb6f3ca0aca08c36b1b2a99dea
Doris <doris@DorisdeMacBook-Air.local>
2023-11-08 09:20:18 +1100

    q1

--- ass2/q1.py
+++ ass2/q1.py
@@ -13,11 +13,21 @@ import re
 db = None
 
 ### process command-line args
-
+selectString = """
+select T.code, count(distinct S.id)
+from Program_enrolments Pe
+join Terms T on Pe.term = T.id
+join Students S on S.id = Pe.student
+where S.status %s 'INTL'
+group by T.code
+"""
 
 try:
   db = psycopg2.connect("dbname=ass2")
-
+  cur = db.cursor()
+  cur.execute(selectString, ["!="])
+  for lStudentCount in cur.fetchall():
+    print(f"{lStudentCount[0]} {lStudentCount[1]}")
   # show term, #locals, #internationals, fraction
 
   # ... add your code here ...
