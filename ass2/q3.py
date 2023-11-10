b584653c63651140fa06134535649aef0224c60f
Doris <doris@DorisdeMacBook-Air.local>
2023-11-10 23:10:03 +1100

    q3

--- ass2/q3.py
+++ ass2/q3.py
@@ -84,6 +84,9 @@ try:
         for course in courseList:
           if '{' in course:
             alternativeC = course.split(';')
+            print(alternativeC)
+            print(alternativeC[0])
+            print(alternativeC[0][1:])
             currCourse = getSubject(db, alternativeC[0][1:])
             print('- ' + course + ' ' + currCourse[2])
             currCourse = getSubject(db, alternativeC[1][1:])
