bbe7aabf5ecb83bb11c0a91696de1a6a0b9d6b5e
DorisDai <dddorishhh@outlook.com>
2023-12-02 22:59:28 +1100

    examprepare

--- finalPrepare/21t3/work/q3/q3.sql
+++ finalPrepare/21t3/work/q3/q3.sql
@@ -5,11 +5,12 @@
 create or replace view q3(id,price,street,suburb)
 as
     -- gives the cheapest unsold house(s).
-    select Pr.id, list_price, St.name, Su.name
-    from Properties as Pr
-    join Streets as St on St.id = Pr.street
-    join suburbs as Su on Su.id = St.suburb
+    select Pr.id, list_price, street_no || ' ' || St.name || ' ' || stype, Su.name
+    from Properties Pr
+    join Streets St on St.id = Pr.street
+    join suburbs Su on Su.id = St.suburb
     where sold_price is null 
-    and list_price = (select min(list_price) from Properties)
+    and ptype = 'House'
+    and list_price = (select min(list_price) from Properties where ptype = 'House' and sold_price is null)
     order by Pr.id
 ;
