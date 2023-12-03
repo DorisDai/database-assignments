b25b2abe9240039abc661010ebedccdca87281ca
DorisDai <dddorishhh@outlook.com>
2023-12-03 12:06:30 +1100

    examprepare

--- finalPrepare/work/q4/q4.sql
+++ finalPrepare/work/q4/q4.sql
@@ -7,9 +7,38 @@ drop function if exists q4();
 drop type if exists SongCounts;
 create type SongCounts as ( "group" text, nshort integer, nlong integer );
 
+create or replace view sScount(Gid, Gname, numS) as
+	select G.id, G.name, count(S.id)
+	from Songs S
+	join Albums A on A.id = S.on_album
+	join Groups G on G.id = A.made_by
+	where S.length < 180
+	group by G.id, G.name
+;
+
+create or replace view lScount(lGid, lGname, numL) as
+	select G.id, G.name, count(S.id)
+	from Songs S
+	join Albums A on A.id = S.on_album
+	join Groups G on G.id = A.made_by
+	where S.length > 360
+	group by G.id, G.name
+;
+
 create or replace function
 	q4() returns setof SongCounts
 as $$
-... PLpgSQL code goes here ...
+declare 
+	_result record;
+begin
+	
+	for _result in 
+		select Gname, numS, numL
+		from lScount join sScount on Gid = lGid
+	loop
+		return next _result
+	end loop;
+
+end;
 $$ language plpgsql
 ;
