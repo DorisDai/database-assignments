b3f29f2d205a0d9033281a959eadf1a4eb3acfe3
DorisDai <dddorishhh@outlook.com>
2023-12-03 16:10:23 +1100

    examprepare

--- finalPrepare/work/q5/q5.sql
+++ finalPrepare/work/q5/q5.sql
@@ -13,20 +13,21 @@ create or replace function
 as $$
 declare
     r1 record;
-    genereString text := '';
+    genereString text;
     result GroupGenres;
 begin
     for r1 in 
-        select G.id as Gid, G.name as Gname, string_agg(A.genre) as gGenre
+        select G.id as Gid, G.name as Gname, string_agg(distinct A.genre, ', ' order by A.genre) as gGenre
         from groups G
-        left join Albums A
-        group by G.id, G.name
+        left join Albums A on G.id = A.made_by
+        group by Gid, Gname
     loop
-        if r1.genre is not null
-            genereString := r1.genre;
+        genereString := '';
+        if r1.gGenre is not null then
+            genereString := r1.gGenre;
         end if;
         result."group" := r1.Gname;
-        result.genres := r1.genereString;
+        result.genres := genereString;
         return next result;
     end loop;
 end;
