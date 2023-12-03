b0e9a4d03592eb34687cf62f46d09f6a066870f1
DorisDai <dddorishhh@outlook.com>
2023-12-03 14:51:16 +1100

    examprepare

--- finalPrepare/work/q4/q4.sql
+++ finalPrepare/work/q4/q4.sql
@@ -7,21 +7,31 @@ drop function if exists q4();
 drop type if exists SongCounts;
 create type SongCounts as ( "group" text, nshort integer, nlong integer );
 
+create or replace view sLthan3Min(sId3, on_album3) as
+	select id, on_album
+	from songs
+	where length < 180
+;
+
+create or replace view sGthan3Min(sId6, on_album6) as
+	select id, on_album
+	from songs
+	where length > 360
+;
+
 create or replace view sScount(Gid, Gname, numS) as
-	select G.id, G.name, count(S.id)
-	from Songs S
-	join Albums A on A.id = S.on_album
+	select G.id, G.name, count(S.sId3)
+	from sLthan3Min S
+	join Albums A on A.id = S.on_album3
 	right join Groups G on G.id = A.made_by
-	where S.length < 180 or S.length is null
 	group by G.id, G.name
 ;
 
 create or replace view lScount(lGid, lGname, numL) as
-	select G.id, G.name, count(S.id)
-	from Songs S
-	join Albums A on A.id = S.on_album
+	select G.id, G.name, count(S.sId6)
+	from sGthan3Min S
+	join Albums A on A.id = S.on_album6
 	right join Groups G on G.id = A.made_by
-	where S.length > 360 or S.length is null
 	group by G.id, G.name
 ;
 
@@ -34,7 +44,7 @@ begin
 	
 	for _result in 
 		select Gname, numS, numL
-		from lScount full join sScount on Gid = lGid
+		from lScount join sScount on Gid = lGid
 	loop
 		return next _result;
 	end loop;
